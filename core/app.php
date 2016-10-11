603561057f0a3ad636832ac52f2a3622112f799e
Clément <clement.lecomte6@orange.fr>
2016-10-11 15:44:40 +0200

    Register DAOUser into the app

--- core/app.php
+++ core/app.php
@@ -2,6 +2,8 @@
 
 require_once __DIR__.'/config/config.php';
 
+// Initialisation of the Database
+
 try{
     $app["connection"] = new PDO(
         "mysql:dbname=".$app["config"]["db"]["BASE"].
@@ -19,5 +21,11 @@ catch(PDOException $e){
     ));
     exit();
 }
+
+// Register DAOUser into the app
+
+$app["DAOUser"] = function ($app) {
+    return new TestApi\DAO\DAOUser($app);
+};
 
 require_once __DIR__.'/route.php';
--- src/DAO/DAOUser.php
+++ src/DAO/DAOUser.php
@@ -5,10 +5,14 @@ namespace TestApi\DAO;
 /**
  * Class DAOUser
  *
- * DataBase Access Object for the model user 
+ * DataBase Access Object for the model user
  */
 class DAOUser
 {
+    protected $app;
 
+    public function __construct($app){
+        $this->app = $app;
+    }
 
 }
