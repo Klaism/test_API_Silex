cee05dbdda8a138975a70bcb94370ebb85b9dc9c
Clément <clement.lecomte6@orange.fr>
2016-10-11 16:06:15 +0200

    creation of getAllUsers()

--- src/DAO/DAOUser.php
+++ src/DAO/DAOUser.php
@@ -2,6 +2,8 @@
 
 namespace TestApi\DAO;
 
+use TestApi\Models\User;
+
 /**
  * Class DAOUser
  *
@@ -13,6 +15,26 @@ class DAOUser
 
     public function __construct($app){
         $this->app = $app;
+    }
+
+    /**
+     * getAll()
+     * get all the users
+     * @return Array list of users
+     */
+    public function getAll(){
+        $res=array();
+        $sql="SELECT * FROM User";
+        $statementUsers=$this->app["connection"]->prepare($sql);
+        $statementUsers->execute();
+
+        while($row=$statementUsers->fetch()){
+            $user=new User($row["name"],$row["email"]);
+            $user->setId($row["id"]);
+            $user->setAge($row["age"]);
+            array_push($res,$user);
+        }
+        return $res;
     }
 
 }
