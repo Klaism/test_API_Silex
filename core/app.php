25390c9a138621eab4ff2e06cef00d6ea4d19697
Clément <clement.lecomte6@orange.fr>
2016-10-11 14:46:05 +0200

    Creation of app.php with the connection

---
+++ core/app.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__.'/config/config.php';
+
+try{
+    $app["connection"] = new PDO(
+        "mysql:dbname=".$app["config"]["db"]["BASE"].
+        ";host=".$app["config"]["db"]["SERVER"],
+        $app["config"]["db"]["USER"],
+        $app["config"]["db"]["PASSWD"]
+    );
+
+}
+catch(PDOException $e){
+    http_response_code (500 );
+    echo json_encode(array(
+        'status' => 500,
+        'error' => "Sql connection failed"
+    ));
+    exit();
+}
--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
-  echo "Test";
+header('Content-type: application/json');
+require_once __DIR__."/vendor/autoload.php";
+
+$app = new Silex\Application();
+require_once __DIR__."/core/app.php";
+
+$app->run();
